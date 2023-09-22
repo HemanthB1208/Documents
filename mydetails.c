5f153022dcc1767c1b1f963045836f00e5e4ac6e
@HemanthB1208 <hemanthhunnt578@gmail.com>
2023-09-22 11:10:10 +0530

    hemanth

---
+++ mydetails.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+printf ("NAME:N.V.S.L.PRAVALLIKA\n");
+printf ("adress:yernagudem\n");
+printf ("Phone number: 9121688739\n");
+printf ("pin code:512125\n");
+printf ("city:rajahmundry");
+return 0;
+}
---
+++ python lab/numpy1.py
@@ -0,0 +1,8 @@
+import numpy as np
+x=np.array([1,2,3,4])
+print(type(x))
+x.shape
+x.ndim
+y=np.array([1,2,3,4])
+y.shape
+y.ndim
